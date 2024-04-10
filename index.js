1561cf9960f2fba657365153b0052b01d928b363
Daniel Rodrigues <danielrodriguescarvalho777@gmail.com>
2024-04-09 21:15:01 -0300

    creating method to increase count

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const app = Vue.createApp({
-    template: '<h1>Olá, {{ name }}!</h1> <button v-on:click="count++">Contador: {{count}}</button>',
+    template: '<h1>Olá, {{ name }}!</h1> <button v-on:click="increaseCount">Contador: {{count}}</button>',
     data(){
         return{
             name: 'Daniel',
             count: 0
+        }
+    },
+    methods:{
+        increaseCount(){
+            this.count++
         }
     }
 })
