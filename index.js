72b39bce02f84782f9017d691edfb6394c738fea
Daniel Rodrigues <danielrodriguescarvalho777@gmail.com>
2024-04-09 21:32:16 -0300

    fetching github api, showing results

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 const app = Vue.createApp({
     data(){
         return{
-            name: 'Daniel',
-            count: 0
+            name: 'Lester',
+            login: 'skibidi',
+            bio: '...',
+            company:'Toilet Inc.',
+            avatar_url:'https://unsplash.it/256',
+            searchInput: ''
+
         }
     },
     methods:{
-        increaseCount(){
-            this.count++
+        async fetchGithubUser(){
+            const res = await fetch(`https://api.github.com/users/${this.searchInput}`)
+            const {login, name, bio, company, avatar_url} = await res.json()
+
+            this.login = login
+            this.name = name
+            this.bio = bio
+            this.company = company
+            this.avatar_url = avatar_url
         }
     }
 })
